5168a99d7d66283cb67d995438667fd858d1fb6e
Jonathan <jjmv219@gmail.com>
2022-02-21 14:07:32 -0500

    Update progBAse.py

--- progBAse.py
+++ progBAse.py
@@ -8,9 +8,9 @@ ventana.geometry("300x300")
 def operacion():
     result.delete(0, tkinter.END)
     result.insert(0, int(txtnumb1.get())+int(txtnumb2.get()))
-    
 
-lbNumb1=tkinter.Label(ventana, text="Numero 1")
+
+lbNumb1 =tkinter.Label(ventana, text="Numero 1")
 lbNumb1.pack()
 txtnumb1=tkinter.Entry(ventana)
 txtnumb1.pack()
